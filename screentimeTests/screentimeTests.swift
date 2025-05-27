2dd70fd28f49482c766c99476838499d74611c4c
Stephen Ni <stephenni1234@gmail.com>
2025-05-27 19:28:26 -0400

    add unit and ui test files

---
+++ screentimeTests/screentimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  screentimeTests.swift
+//  screentimeTests
+//
+//  Created by Stephen Ni on 2025-05-27.
+//
+
+import Testing
+@testable import screentime
+
+struct screentimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
